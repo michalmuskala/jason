4166ee960519c91178188b844046147f458217ee
Michal Muskala <michal@muskala.eu>
2017-06-13 21:12:41 +0200

    Semi-working strings

--- lib/parser.ex
+++ lib/parser.ex
@@ -1,6 +1,8 @@
 # TODO: guard against too large floats
 
 defmodule Antidote.Parser do
+  import Bitwise
+
   def parse(data) when is_binary(data) do
     value(data, data, 0, [:terminate])
   end
@@ -17,7 +19,7 @@ defmodule Antidote.Parser do
     &:orddict.merge(merge, &1, &2))
   dispatch = Enum.with_index(:array.to_list(:array.from_orddict(orddict, :error)))
 
-  for {action, byte} <- dispatch, not action in [:number, :number_zero] do
+  for {action, byte} <- dispatch, not action in [:number, :number_zero, :string] do
     defp value(<<unquote(byte), rest::bits>>, original, skip, stack) do
       unquote(action)(rest, original, skip + 1, stack)
     end
@@ -26,6 +28,9 @@ defmodule Antidote.Parser do
     defp value(<<unquote(byte), rest::bits>>, original, skip, stack) do
       number(rest, original, skip, stack, 1)
     end
+  end
+  defp value(<<?\", rest::bits>>, original, skip, stack) do
+    string(rest, original, skip + 1, stack, 0)
   end
   defp value(<<?0, rest::bits>>, original, skip, stack) do
     number_zero(rest, original, skip, stack)
@@ -171,8 +176,80 @@ defmodule Antidote.Parser do
     error(original, skip)
   end
 
-  defp string(<<rest::bits>>, original, skip, stack) do
-    raise "not supported"
+  defp string(<<?\", rest::bits>>, original, skip, stack, len) do
+    string = binary_part(original, skip, len)
+    continue(rest, original, skip + len + 1, stack, string)
+  end
+  defp string(<<?\\, rest::bits>>, original, skip, stack, len) do
+    part = binary_part(original, skip, len)
+    escape(rest, original, skip + len, stack, part)
+  end
+  # TODO: validate more tightly
+  defp string(<<_byte, rest::bits>>, original, skip, stack, len) do
+    string(rest, original, skip, stack, len + 1)
+  end
+  # defp string(<<_rest::bits>>, original, skip, _stack, _len) do
+  #   error(original, skip)
+  # end
+
+  defp string(<<?\", rest::bits>>, original, skip, stack, acc, len) do
+    last = binary_part(original, skip, len)
+    string = IO.iodata_to_binary([acc | last])
+    continue(rest, original, skip + len + 1, stack, string)
+  end
+  defp string(<<?\\, rest::bits>>, original, skip, stack, acc, len) do
+    part = binary_part(original, skip, len)
+    escape(rest, original, skip + len, stack, [acc | part])
+  end
+  # TODO: validate more tightly
+  defp string(<<_byte, rest::bits>>, original, skip, stack, acc, len) do
+    string(rest, original, skip, stack, acc, len + 1)
+  end
+  # defp string(<<_rest::bits>>, original, skip, _stack, _acc, _len) do
+  #   error(original, skip)
+  # end
+
+  escapes = Enum.zip('\b\t\n\f\r"\\/', 'btnfr"\\/')
+
+  for {byte, escape} <- escapes do
+    defp escape(<<unquote(byte), rest::bits>>, original, skip, stack, acc) do
+      string(rest, original, skip + 2, stack, [acc, unquote(escape)], 0)
+    end
+  end
+  defp escape(<<?u, rest::bits>>, original, skip, stack, acc) do
+    escapeu(rest, original, skip, stack, acc)
+  end
+  defp escape(<<_rest::bits>>, original, skip, _stack, _acc) do
+    error(original, skip)
+  end
+
+  defp escapeu(<<a1, b1, c1, d1, ?\\, ?u, a2, b2, c2, d2, rest::bits>>, original, skip, stack, acc)
+       when a1 in 'dD' and a2 in 'dD'
+       and (b1 in '89abAB')
+       and (b2 in ?c..?f or b2 in ?C..?F) do
+    try do
+      hi = List.to_integer([a1, b1, c1, d1], 16)
+      lo = List.to_integer([a2, b2, c2, d2], 16)
+      {hi, lo}
+    rescue
+      ArgumentError ->
+        raise "error"
+    else
+      {hi, lo} ->
+        codepoint = 0x10000 + ((hi &&& 0x03FF) <<< 10) + (lo &&& 0x03FF)
+        string(rest, original, skip + 12, stack, [acc, <<codepoint::utf8>>], 0)
+    end
+  end
+  defp escapeu(<<escape::binary-4, rest::bits>>, original, skip, stack, acc) do
+    try do
+      String.to_integer(escape, 16)
+    rescue
+      ArgumentError ->
+        raise "error"
+    else
+      codepoint ->
+        string(rest, original, skip + 6, stack, [acc, <<codepoint::utf8>>], 0)
+    end
   end
 
   defp error(<<_rest::bits>>, original, skip, _stack) do
