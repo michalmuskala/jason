f9d227716c2983918d210402b759f871300399da
Michal Muskala <michal@muskala.eu>
2017-06-17 12:09:50 +0200

    Add interface to encoder

--- lib/antidote.ex
+++ lib/antidote.ex
@@ -9,4 +9,33 @@ defmodule Antidote do
       {:error, error} -> raise error
     end
   end
+
+  def encode(input, opts \\ []) do
+    case Antidote.Encode.encode(input, format_opts(opts)) do
+      {:ok, result} -> {:ok, IO.iodata_to_binary(result)}
+      {:error, error} -> {:error, error}
+    end
+  end
+
+  def encode!(input, opts \\ []) do
+    case Antidote.Encode.encode(input, format_opts(opts)) do
+      {:ok, result} -> IO.iodata_to_binary(result)
+      {:error, error} -> raise error
+    end
+  end
+
+  def encode_to_iodata(input, opts \\ []) do
+    Antidote.Encode.encode(input, format_opts(opts))
+  end
+
+  def encode_to_iodata!(input, opts \\ []) do
+    case Antidote.Encode.encode(input, format_opts(opts)) do
+      {:ok, result} -> result
+      {:error, error} -> raise error
+    end
+  end
+
+  defp format_opts(opts) do
+    Enum.into(opts, %{escape: :json, validate: true, maps: :naive})
+  end
 end
