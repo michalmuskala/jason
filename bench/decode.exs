2a29806e07317edcb908ddbe485a61bd8b5b95e5
Clark Kampfe <clark.kampfe@gmail.com>
2018-02-08 17:51:52 -0600

    add :erlang.binary_to_term/1 to decode benchmark

--- bench/decode.exs
+++ bench/decode.exs
@@ -1,4 +1,4 @@
-decode_jobs = %{
+json_decode_jobs = %{
   "Jason" => &Jason.decode!/1,
   "Poison"   => &Poison.decode!/1,
   "JSX"      => &JSX.decode!(&1, [:strict]),
@@ -6,6 +6,10 @@ decode_jobs = %{
   "jsone"    => &:jsone.decode/1,
   "jiffy"    => &:jiffy.decode(&1, [:return_maps, :use_nil]),
   "JSON"     => &JSON.decode!/1,
+}
+
+erlang_term_to_binary_decode_jobs = %{
+  "binary_to_term/1" => &:erlang.binary_to_term/1,
 }
 
 decode_inputs = [
@@ -21,25 +25,62 @@ decode_inputs = [
   "Issue 90",
 ]
 
-read_data = fn (name) ->
+read_json_data = fn (name) ->
   name
   |> String.downcase
   |> String.replace(~r/([^\w]|-|_)+/, "-")
   |> String.trim("-")
-  |> (&"data/#{&1}.json").()
+  |> (&"data/json/#{&1}.json").()
   |> Path.expand(__DIR__)
   |> File.read!
 end
 
-Benchee.run(decode_jobs,
+read_erlang_binary_data = fn (name) ->
+  name
+  |> String.downcase
+  |> String.replace(~r/([^\w]|-|_)+/, "-")
+  |> String.trim("-")
+  |> (&"data/binary/#{&1}.binary").()
+  |> Path.expand(__DIR__)
+  |> File.read!
+end
+
+json_inputs =
+  for name <- decode_inputs, into: %{} do
+    name
+    |> read_json_data.()
+    |> (&{name, &1}).()
+  end
+
+json_benchmarks =
+  for {input_name, input_data} <- json_inputs,
+      {job_name, job_fn} <- json_decode_jobs,
+      into: %{}
+  do
+    {input_name <> " " <> job_name, fn() -> job_fn.(input_data) end}
+  end
+
+erlang_binary_inputs =
+  for name <- decode_inputs, into: %{} do
+    name
+    |> read_erlang_binary_data.()
+    |> (&{name, &1}).()
+  end
+
+erlang_term_to_binary_benchmarks =
+  for {input_name, input_data} <- erlang_binary_inputs,
+      {job_name, job_fn} <- erlang_term_to_binary_decode_jobs,
+      into: %{}
+  do
+    {input_name <> " " <> job_name, fn() -> job_fn.(input_data) end}
+  end
+
+all_benchmarks = Map.merge(json_benchmarks, erlang_term_to_binary_benchmarks)
+
+Benchee.run(all_benchmarks,
   parallel: 4,
   warmup: 5,
   time: 30,
-  inputs: for name <- decode_inputs, into: %{} do
-    name
-    |> read_data.()
-    |> (&{name, &1}).()
-  end,
   formatters: [
     &Benchee.Formatters.HTML.output/1,
     &Benchee.Formatters.Console.output/1,
