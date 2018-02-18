d70f671f01a3611430feb6bfc30ba130b93fdbb3
Clark Kampfe <clark.kampfe@gmail.com>
2018-02-18 16:26:50 -0600

    Add term_to_binary and binary_to_term to benchmarks (#23)
    * add :erlang.term_to_binary/1 to encode benchmark
    
    * add :erlang.binary_to_term/1 to decode benchmark
    
    * fix encode.exs data path
    
    * fixup in response to PR comments

--- bench/decode.exs
+++ bench/decode.exs
@@ -1,11 +1,12 @@
 decode_jobs = %{
-  "Jason" => &Jason.decode!/1,
-  "Poison"   => &Poison.decode!/1,
-  "JSX"      => &JSX.decode!(&1, [:strict]),
-  "Tiny"     => &Tiny.decode!/1,
-  "jsone"    => &:jsone.decode/1,
-  "jiffy"    => &:jiffy.decode(&1, [:return_maps, :use_nil]),
-  "JSON"     => &JSON.decode!/1,
+  "Jason"  => fn {json, _} -> Jason.decode!(json) end,
+  "Poison" => fn {json, _} -> Poison.decode!(json) end,
+  "JSX"    => fn {json, _} -> JSX.decode!(json, [:strict]) end,
+  "Tiny"   => fn {json, _} -> Tiny.decode!(json) end,
+  "jsone"  => fn {json, _} -> :jsone.decode(json) end,
+  "jiffy"  => fn {json, _} -> :jiffy.decode(json, [:return_maps, :use_nil]) end,
+  "JSON"   => fn {json, _} -> JSON.decode!(json) end,
+  "binary_to_term/1" => fn {_, etf} -> :erlang.binary_to_term(etf) end,
 }
 
 decode_inputs = [
@@ -22,24 +23,25 @@ decode_inputs = [
 ]
 
 read_data = fn (name) ->
-  name
-  |> String.downcase
-  |> String.replace(~r/([^\w]|-|_)+/, "-")
-  |> String.trim("-")
-  |> (&"data/#{&1}.json").()
-  |> Path.expand(__DIR__)
-  |> File.read!
+  file =
+    name
+    |> String.downcase
+    |> String.replace(~r/([^\w]|-|_)+/, "-")
+    |> String.trim("-")
+
+  json = File.read!(Path.expand("data/#{file}.json", __DIR__))
+  etf = :erlang.term_to_binary(Jason.decode!(json))
+
+  {json, etf}
 end
+
+inputs = for name <- decode_inputs, into: %{}, do: {name, read_data.(name)}
 
 Benchee.run(decode_jobs,
   parallel: 4,
   warmup: 5,
   time: 30,
-  inputs: for name <- decode_inputs, into: %{} do
-    name
-    |> read_data.()
-    |> (&{name, &1}).()
-  end,
+  inputs: inputs,
   formatters: [
     &Benchee.Formatters.HTML.output/1,
     &Benchee.Formatters.Console.output/1,
