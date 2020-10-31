2e3b5f519af1adab1692e2808d5f160613e844c4
Michał Muskała <michal@muskala.eu>
2020-10-31 21:42:00 +0000

    Make ex_doc regular dev dependency

--- mix.exs
+++ mix.exs
@@ -29,7 +29,7 @@ defmodule Jason.Mixfile do
     [
       {:decimal, "~> 1.0 or ~> 2.0", optional: true},
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.18", only: :docs},
+      {:ex_doc, "~> 0.18", only: :dev, runtime: false},
     ] ++ maybe_stream_data()
   end
 
