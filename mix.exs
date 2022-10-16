85202398577e7d7ed1581da2bc2512bf6aeeb6a6
Michał Muskała <michal@muskala.eu>
2022-10-16 22:03:45 +0100

    Try another way to fix CI

--- mix.exs
+++ mix.exs
@@ -30,16 +30,9 @@ defmodule Jason.Mixfile do
     [
       {:decimal, "~> 1.0 or ~> 2.0", optional: true},
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
-      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
-    ] ++ maybe_jason_native() ++ maybe_stream_data()
-  end
-
-  defp maybe_jason_native() do
-    if System.get_env("NO_JASON_NATIVE") do
-      []
-    else
-      [{:jason_native, ">= 0.0.0", optional: true}]
-    end
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:jason_native, ">= 0.0.0", optional: true}
+    ] ++ maybe_stream_data()
   end
 
   defp maybe_stream_data() do
