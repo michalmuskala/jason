ed8f1c2dee04e13a103880c40e838750e238a712
Thibaut Barrère <thibaut.barrere@gmail.com>
2023-05-21 15:09:24 +0200

    Document Jason.Fragment (#166)
    * Add a test
    
    * Document Jason.Fragment
    
    * Add something in the readme

--- lib/fragment.ex
+++ lib/fragment.ex
@@ -1,4 +1,14 @@
 defmodule Jason.Fragment do
+  @moduledoc ~S"""
+  Provides a way to inject an already-encoded JSON structure into a
+  to-be-encoded structure in optimized fashion.
+
+  This avoids a decoding/encoding round-trip for the subpart.
+
+  This feature can be used for caching parts of the JSON, or delegating
+  the generation of the JSON to a third-party system (e.g. Postgres).
+  """
+
   defstruct [:encode]
 
   def new(iodata) when is_list(iodata) or is_binary(iodata) do
